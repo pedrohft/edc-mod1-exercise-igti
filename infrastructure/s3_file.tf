57de452b3d6aed791bc3355bed59585805a75449
pedroteixeirazup <pedro.teixeira@zup.com.br>
2021-08-15 11:42:47 -0300

    add spark job

--- infrastructure/s3_file.tf
+++ infrastructure/s3_file.tf
@@ -1,8 +1,8 @@
 
 resource "aws_s3_bucket_object" "docentes_co" {
   bucket = aws_s3_bucket.dl.id
-  key    = "raw/docentes_co.csv"
+  key    = "glue/pyspark/job_spark_from_tf.py"
   acl    = "private"
-  source = "C:/Users/gloom/Documents/IGTI/desafio-modulo-01/data/microdados_censo_escolar_2020/microdados_educacao_basica_2020/DADOS/docentes_co.CSV"
-  etag   = filemd5("C:/Users/gloom/Documents/IGTI/desafio-modulo-01/data/microdados_censo_escolar_2020/microdados_educacao_basica_2020/DADOS/docentes_co.CSV")
+  source = "../job_spark.py"
+  etag   = filemd5("../job_spark.py")
 }
