dd16e1285080bdf5fa3ccffd641ef5af0c8b02dc
pedroteixeirazup <pedro.teixeira@zup.com.br>
2021-08-15 10:52:38 -0300

    adicionando s3 file.

--- infrastructure/s3_file.tf
+++ infrastructure/s3_file.tf
@@ -1,4 +1,8 @@
 
-provider "aws" {
-  region = "${var.aws_region}"
+resource "aws_s3_bucket_object" "docentes_co" {
+  bucket = aws_s3_bucket.dl.id
+  key    = "raw/docentes_co.csv"
+  acl    = "private"
+  source = "C:/Users/gloom/Documents/IGTI/desafio-modulo-01/data/microdados_censo_escolar_2020/microdados_educacao_basica_2020/DADOS/docentes_co.CSV"
+  etag   = filemd5("C:/Users/gloom/Documents/IGTI/desafio-modulo-01/data/microdados_censo_escolar_2020/microdados_educacao_basica_2020/DADOS/docentes_co.CSV")
 }
